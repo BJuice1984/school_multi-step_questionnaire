8405202b41c35e57d3484f6e3cc601933eee99f6
BJuice1984 <beetle_juice84@mail.ru>
2024-10-12 21:18:56 +0300

    feat(app, HomeClient) add nickname submit logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import HomeClient from '@/components/HomeClient'
 
 export default function Home() {
     return (
         <Box p={8}>
-            <Heading mb={4}>Тест для проверки и подготовки к ЕГЭ</Heading>
+            <Heading as='h1' size='xl' mb={4}>
+                Тест для проверки и подготовки к ЕГЭ
+            </Heading>
             <HomeClient />
         </Box>
     )
--- components/HomeClient.tsx
+++ components/HomeClient.tsx
@@ -1,11 +1,23 @@
 'use client'
 
 import { useState, useEffect, useRef } from 'react'
-import { Box, Button, Input, Text, Heading } from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    Input,
+    Text,
+    Heading,
+    FormControl,
+    FormHelperText,
+    Stack,
+} from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
 
 export default function HomeClient() {
-    const [nickname, setNickname] = useState<string | null>(null)
+    const [nickname, setNickname] = useState<string | null>(
+        sessionStorage.getItem('nickname') ?? null
+    )
     const nicknameInputRef = useRef<HTMLInputElement | null>(null)
     const router = useRouter()
 
@@ -16,21 +28,28 @@ export default function HomeClient() {
         }
     }, [])
 
-    const handleStart = () => {
-        if (nickname && nickname.trim() !== '') {
-            sessionStorage.setItem('nickname', nickname)
-            router.push('/test')
+    const {
+        handleSubmit,
+        formState: { isSubmitting },
+    } = useForm()
+
+    const onSubmit = () => {
+        const newNickname = nicknameInputRef.current?.value || ''
+
+        if (newNickname && newNickname.trim() !== '') {
+            setNickname(newNickname)
+            sessionStorage.setItem('nickname', newNickname)
         }
     }
 
-    const handleNicknameChange = () => {
-        const newNickname = nicknameInputRef.current?.value || ''
-        setNickname(newNickname)
+    const onExit = () => {
+        setNickname(null)
+        sessionStorage.removeItem('nickname')
     }
 
     return (
         <Box>
-            <Heading mb={4}>
+            <Heading as='h2' size='md' mb={4}>
                 {nickname && nickname.trim() !== ''
                     ? `Добро пожаловать, ${nickname}! Нажмите Начать для прохождения теста`
                     : 'Добро пожаловать! Придумайте свой ник'}
@@ -41,22 +60,47 @@ export default function HomeClient() {
                     : ''}
             </Text>
             {!nickname && (
-                <Input
-                    ref={nicknameInputRef}
-                    placeholder='Введите свой никнейм'
-                    onChange={handleNicknameChange}
-                    mb={4}
-                    _placeholder={{ opacity: 1, color: 'gray.500' }}
-                />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormControl>
+                        <Stack direction='row' spacing={4}>
+                            <Input
+                                ref={nicknameInputRef}
+                                placeholder='Введите свой никнейм'
+                                mb={4}
+                                _placeholder={{ opacity: 1, color: 'gray.500' }}
+                            />
+                            <Button
+                                isLoading={isSubmitting}
+                                colorScheme='teal'
+                                variant='solid'
+                                type='submit'
+                            >
+                                Ok
+                            </Button>
+                        </Stack>
+                        <FormHelperText>Введите свой никнейм</FormHelperText>
+                    </FormControl>
+                </form>
             )}
-            <Button
-                colorScheme='teal'
-                onClick={handleStart}
-                mb={4}
-                isDisabled={!nickname || nickname.trim() === ''}
-            >
-                Начать
-            </Button>
+            <Stack direction='row' spacing={4}>
+                <Button
+                    colorScheme='teal'
+                    onClick={() => router.push('/test')}
+                    mb={4}
+                    isDisabled={!nickname || nickname.trim() === ''}
+                >
+                    Начать
+                </Button>
+
+                <Button
+                    colorScheme='teal'
+                    onClick={() => onExit()}
+                    mb={4}
+                    isDisabled={!nickname || nickname.trim() === ''}
+                >
+                    Выход
+                </Button>
+            </Stack>
         </Box>
     )
 }
